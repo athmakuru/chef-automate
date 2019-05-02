0195c6aaf72ff045a1551219df2be229e3b883aa
Maggie Walker <magwalk@gmail.com>
2019-05-02 12:22:02 -0700

    Small tweaks to projects filter dropdown
    Signed-off-by: Maggie Walker <magwalk@gmail.com>

--- components/automate-ui/src/app/page-components/projects-filter-dropdown/projects-filter-dropdown.spec.ts
+++ components/automate-ui/src/app/page-components/projects-filter-dropdown/projects-filter-dropdown.spec.ts
@@ -135,7 +135,7 @@ describe('ProjectsFilterDropdownComponent', () => {
 
       it('the dropdown is hidden', () => {
         const dropdown = fixture.nativeElement.querySelector('.dropdown');
-        expect(dropdown).toBeNull();
+        expect(dropdown.getAttribute('visible')).toEqual('false');
       });
     });
 
